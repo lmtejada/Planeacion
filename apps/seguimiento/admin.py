213362fa1f5e0196941a3a55d88e0d3fc6c3b6a5
lmtejada <laura_mtc@hotmail.com>
2016-11-29 23:17:45 -0500

    Crear el resto de migraciones y agregarlas a la interfaz de administracion

--- apps/seguimiento/admin.py
+++ apps/seguimiento/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from apps.seguimiento.models import Entidad,Persona, PoliticaPublica, Indicador, Formulario, Pregunta
 
-# Register your models here.
+admin.site.register(Entidad)
+admin.site.register(Persona)
+admin.site.register(PoliticaPublica)
+admin.site.register(Indicador)
+admin.site.register(Formulario)
+admin.site.register(Pregunta)
--- apps/seguimiento/models.py
+++ apps/seguimiento/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-
 class Entidad(models.Model):
 	id = models.IntegerField(primary_key=True)
 	nombre = models.CharField(max_length=200)
@@ -22,16 +20,25 @@ class Indicador(models.Model):
 	nombre = models.CharField(max_length=200)
 	politica_publica = models.ManyToManyField(PoliticaPublica) 
 	entidad = models.ManyToManyField(Entidad)
-'''
+
 class Formulario(models.Model):
 	id = models.AutoField(primary_key=True)
-	idPolitica = models.ForeignKey(Politicapublica, on_delete=models.CASCADE)
+	politica = models.OneToOneField(PoliticaPublica, null=True, on_delete=models.SET_NULL)
+
+class FormularioRespuesta(models.Model):
+	id = models.AutoField(primary_key=True)
+	fecha = models.DateTimeField(auto_now_add=True)
+	entidad = models.OneToOneField(Entidad, on_delete=models.PROTECT)
+	indicador = models.OneToOneField(Indicador, on_delete=models.PROTECT)
 
 class Pregunta(models.Model):
 	id = models.AutoField(primary_key=True)
-	idPolitica = models.ForeignKey(Politicapublica, on_delete=models.CASCADE)
+	enunciado = models.TextField()
+	formulario = models.ForeignKey(Formulario, on_delete=models.PROTECT)
 
 class Respuesta(models.Model):
 	id = models.AutoField(primary_key=True)
-	idPolitica = models.ForeignKey(Politicapublica, on_delete=models.CASCADE)
-'''
+	valor = models.TextField()
+	pregunta = models.OneToOneField(Pregunta, on_delete=models.PROTECT)
+	formulario_respuesta = models.ForeignKey(FormularioRespuesta, null=True, on_delete=models.PROTECT)
+
