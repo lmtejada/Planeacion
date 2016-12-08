2d827921e511d766932e08fb2d2f9cf2e29044d8
danielflorez111 <danielflorez111@hotmail.com>
2016-12-08 15:55:30 -0500

    Formulario

--- apps/seguimiento/views.py
+++ apps/seguimiento/views.py
@@ -4,9 +4,9 @@ from django.views.generic import CreateView
 #from apps.seguimiento.forms import RespuestaForm
 from django.core.urlresolvers import reverse_lazy
 from apps.login.models import Persona
-from apps.seguimiento.models import Entidad, Indicador, Pregunta, Respuesta, PoliticaPublica
+from apps.seguimiento.models import Entidad, Indicador, Formulario, Pregunta, Respuesta, PoliticaPublica
 from apps.seguimiento.forms import FormularioRespuestaForm
-from django.http import QueryDict
+from django.db.models import prefetch_related_objects
 
 # Create your views here.
 
@@ -22,28 +22,35 @@ def form_view(request):
 	title = "Registrar formulario"
 	form = FormularioRespuestaForm(request.POST or None)
 	respuestas = (request.POST.keys() or None)
-	preguntas = Pregunta.objects.all()
-	indicadores = Indicador.objects.all()
+	usuario = request.user
+	persona = Persona.objects.filter(user=usuario).select_related('entidad').first()
+	formulario = Formulario.objects.filter(id=1).prefetch_related('pregunta_set').first()
+	preguntas = formulario.pregunta_set.all()
+	indicadores = Indicador.objects.filter(entidad=persona.entidad)
+	prefetch_related_objects(indicadores, 'politica_publica')
+	politicas = []
+	for indicador in indicadores:
+		if indicador.politica_publica not in politicas:
+			politicas.append(indicador.politica_publica)
+
 	if form.is_valid():
 		formularioRespuesta = form.save(commit=False)
-		usuario = request.user 
-		persona = Persona.objects.filter(user=usuario.id).first()
-		entidad = Entidad.objects.filter(nombre=persona.entidad).first()
-		formularioRespuesta.entidad = entidad
+		formularioRespuesta.entidad = persona.entidad
 		formularioRespuesta.save()
 
 		for i in respuestas:
 			if i.startswith('valor_'):
 				valor = request.POST[i]
-				data = i.split('_', 1)
-				pregunta = Pregunta.objects.filter(id=data[1]).first()
-				respuesta = Respuesta(valor=valor, pregunta=pregunta, formulario_respuesta=formularioRespuesta)
-				respuesta.save()
-
-				
-		return redirect('seguimiento:index')
+				if valor != '':
+					data = i.split('_', 1)
+					pregunta = Pregunta.objects.filter(id=data[1]).first()
+					respuesta = Respuesta(valor=valor, pregunta=pregunta, formulario_respuesta=formularioRespuesta)
+					respuesta.save()
+		
+		return redirect('cuenta:home')
 
 	return render(request, "seguimiento/formulario.html", {"form": form, 
+												   "politicas": politicas,
 												   "indicadores": indicadores, 
 												   "preguntas": preguntas, 
 												   "title": title})
