844205e632278519d8cf5be31c1494ef20463d80
danielflorez111 <danielflorez111@hotmail.com>
2016-12-11 23:32:30 -0500

    Consultar indicadores

--- apps/consultas/views.py
+++ apps/consultas/views.py
@@ -2,17 +2,25 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from apps.seguimiento.models import FormularioRespuesta
+from apps.login.models import Persona
 
 # Create your views here.
 
 @login_required()
 def listado_view(request):
+
+
 	if request.user.groups.filter(name='Administrador').count() == 1:
 		extends = 'base/admin_nav.html'
-	else:
-		extends = 'base/user_nav.html'
-
-	formulariorespuestas = FormularioRespuesta.objects.all()
-
-	return render(request, "consultas/listado_formularios.html", {"extends": extends,
+		formulariorespuestas = FormularioRespuesta.objects.filter(enviado=True)
+		return render(request, "consultas/form_list_admin.html", {"extends": extends,
 																"formulariorespuestas": formulariorespuestas})
+	elif request.user.groups.filter(name='Operador').count() == 1:
+		extends = 'base/user_nav.html'
+		usuario = request.user
+		persona = Persona.objects.filter(user=usuario).select_related('entidad').first()
+		formulariorespuestas = FormularioRespuesta.objects.filter(entidad=persona.entidad)
+		return render(request, "consultas/form_list_user.html", {"extends": extends,
+																"formulariorespuestas": formulariorespuestas})
+
+	return redirect('cuenta:home')
