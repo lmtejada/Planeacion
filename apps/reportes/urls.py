a5b2ccf696898b454ec1e37692df1b38e6e35a2c
danielflorez111 <danielflorez111@hotmail.com>
2016-12-25 23:48:20 -0500

    Organizar listado y reporte general

--- apps/reportes/urls.py
+++ apps/reportes/urls.py
@@ -1,13 +1,8 @@
 from django.conf.urls import url
 from apps.reportes.views import (
 	reporte_general_view,
-	reporte_general2_view,
-	reporte_general3_view,
 )
 
 urlpatterns = [
-
-    url(r'^general/', reporte_general_view, name='general'),
-    url(r'^general2/', reporte_general2_view, name='general2'),
-    url(r'^general3/', reporte_general3_view, name='general3'),
+	url(r'^general/', reporte_general_view, name='general'),
 ]
--- apps/reportes/views.py
+++ apps/reportes/views.py
@@ -52,15 +52,33 @@ def reporte_general_view(request):
 	}
 
 	for i in formularios:
+		politica = PoliticaPublica.objects.filter(formulariorespuesta__id=i.id).first()
+		respuestas[politica.id] = {}
 		for j in indicadores:
-			respuestas[j.id] = i.respuesta_set.filter(indicador=j).order_by('pregunta')
+			respuestas[politica.id][j.id] = i.respuesta_set.filter(indicador=j).filter(indicador__politica_publica=politica.id).order_by('pregunta')
 			niveles[j.politica_publica.id] = {}
+
+			print (respuestas[politica.id][j.id])
+			
 			nivel1 = Indicador.objects.filter(id=j.id).select_related('nivel1').first()
-			niveles[j.politica_publica.id]['nivel1'] = niveles1[nivel1.nivel1.nivel]
+			if nivel1.nivel1 is not None:
+				niveles[j.politica_publica.id]['nivel1'] = niveles1[nivel1.nivel1.nivel]
+			else: 
+				niveles[j.politica_publica.id]['nivel1'] = "Nivel 1"
+			
 			nivel2 = Indicador.objects.filter(id=j.id).select_related('nivel2').first()
-			niveles[j.politica_publica.id]['nivel2'] = niveles2[nivel2.nivel2.nivel]
+			if nivel2.nivel2 is not None:
+				niveles[j.politica_publica.id]['nivel2'] = niveles2[nivel2.nivel2.nivel]
+			else: 
+				niveles[j.politica_publica.id]['nivel2'] = "Nivel 2"
+
 			nivel3 = Indicador.objects.filter(id=j.id).select_related('nivel3').first()
-			niveles[j.politica_publica.id]['nivel3'] = niveles3[nivel3.nivel3.nivel]
+			if nivel3.nivel3 is not None:
+				niveles[j.politica_publica.id]['nivel3'] = niveles3[nivel3.nivel3.nivel]
+			else:
+				niveles[j.politica_publica.id]['nivel3'] = "Nivel 3"
+
+	#print (respuestas)
 	
 	if request.user.groups.filter(name='Administrador').count() == 1:
 		extends = 'base/admin_nav.html'
@@ -75,21 +93,3 @@ def reporte_general_view(request):
 													 "respuestas": respuestas,
 													 "niveles": niveles
 													 })
-
-
-def reporte_general2_view(request):	
-	if request.user.groups.filter(name='Administrador').count() == 1:
-		extends = 'base/admin_nav.html'
-	elif request.user.groups.filter(name='Operador').count() == 1:
-		extends = 'base/user_nav.html'
-		
-	return render(request, "reportes/general2.html", {"extends": extends})
-
-
-def reporte_general3_view(request):	
-	if request.user.groups.filter(name='Administrador').count() == 1:
-		extends = 'base/admin_nav.html'
-	elif request.user.groups.filter(name='Operador').count() == 1:
-		extends = 'base/user_nav.html'
-		
-	return render(request, "reportes/general3.html", {"extends": extends})
