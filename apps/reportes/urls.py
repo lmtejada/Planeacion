9ca3ab0e79f55090ad827f50e849dba8761a9d90
lmtejada <laura_mtc@hotmail.com>
2016-12-27 21:38:53 -0500

    Reportes por política y por nivel 1

--- apps/reportes/urls.py
+++ apps/reportes/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
 from apps.reportes.views import (
 	reporte_general_view,
+	reporte_politicas_view,
+	reporte_nivel1_view
 )
 
 urlpatterns = [
 	url(r'^general/', reporte_general_view, name='general'),
+	url(r'^politicas/', reporte_politicas_view, name='politicas'),
+	url(r'^nivel1/', reporte_nivel1_view, name='nivel1'),
 ]
--- apps/reportes/views.py
+++ apps/reportes/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.db.models import prefetch_related_objects
-from apps.seguimiento.models import FormularioRespuesta, Indicador, Respuesta, Pregunta, PoliticaPublica, Vigencia
+from django.db.models import prefetch_related_objects, Sum
+from django.db import connection
+from apps.seguimiento.models import FormularioRespuesta, Indicador, Respuesta, Pregunta, PoliticaPublica, Vigencia, EjeEstrategico, Programa, Subprograma, Nivel1, Nivel2, Nivel3
 from apps.login.models import Persona
 
 @login_required()
@@ -25,60 +26,20 @@ def reporte_general_view(request):
 
 	vigencia = Vigencia.objects.filter(activo=True).select_related('formulario').first()
 	headers = Pregunta.objects.filter(formulario=vigencia.formulario).order_by('id')
-	formularios = FormularioRespuesta.objects.filter(vigencia=vigencia).prefetch_related('respuesta_set')
+	formularios = FormularioRespuesta.objects.filter(vigencia=vigencia).filter(estado='aprobado').prefetch_related('respuesta_set')
 	respuestas = {}
-	niveles = {}
-
-	niveles1 = {
-        '1':'Línea orientadora',
-		'2':'Línea estratégica',
-		'3':'Categoría',
-		'4':'Objetivo estratégico',
-		'5':'Estrategia',
-		'6':'Objetivo de la política',
-	}
-
-	niveles2 = {
-	    '1': 'Categoría de la política',
-	    '2': 'Estrategia',
-	    '3': 'Objetivo de la política',
-	    '4': 'Acciones recomendadas',
-	}
-
-	niveles3 = {
-	    '1': 'Objetivo de la política',
-	    '2': 'Acciones recomendadas',
-	    '3': 'Estrategia',
-	}
 
 	for i in formularios:
-		politica = PoliticaPublica.objects.filter(formulariorespuesta__id=i.id).first()
-		respuestas[politica.id] = {}
-		for j in indicadores:
-			respuestas[politica.id][j.id] = i.respuesta_set.filter(indicador=j).filter(indicador__politica_publica=politica.id).order_by('pregunta')
-			niveles[j.politica_publica.id] = {}
+		for j in politicas:
+			if i.politica_publica == j:
+				respuestas[j.id] = {}
+				for k in indicadores:
+					respuestas[j.id][k.id] = i.respuesta_set.filter(indicador=k).filter(indicador__politica_publica=j).order_by('pregunta')
 
-			print (respuestas[politica.id][j.id])
-			
-			nivel1 = Indicador.objects.filter(id=j.id).select_related('nivel1').first()
-			if nivel1.nivel1 is not None:
-				niveles[j.politica_publica.id]['nivel1'] = niveles1[nivel1.nivel1.nivel]
-			else: 
-				niveles[j.politica_publica.id]['nivel1'] = "Nivel 1"
-			
-			nivel2 = Indicador.objects.filter(id=j.id).select_related('nivel2').first()
-			if nivel2.nivel2 is not None:
-				niveles[j.politica_publica.id]['nivel2'] = niveles2[nivel2.nivel2.nivel]
-			else: 
-				niveles[j.politica_publica.id]['nivel2'] = "Nivel 2"
+	ejes = EjeEstrategico.objects.all()
+	programas = Programa.objects.all()
+	subprogramas = Subprograma.objects.all()
 
-			nivel3 = Indicador.objects.filter(id=j.id).select_related('nivel3').first()
-			if nivel3.nivel3 is not None:
-				niveles[j.politica_publica.id]['nivel3'] = niveles3[nivel3.nivel3.nivel]
-			else:
-				niveles[j.politica_publica.id]['nivel3'] = "Nivel 3"
-
-	#print (respuestas)
 	
 	if request.user.groups.filter(name='Administrador').count() == 1:
 		extends = 'base/admin_nav.html'
@@ -91,5 +52,90 @@ def reporte_general_view(request):
 													 "indicadores": indicadores,
 													 "headers": headers,
 													 "respuestas": respuestas,
-													 "niveles": niveles
+													 "ejes": ejes,
+													 "programas": programas,
+													 "subprogramas": subprogramas
+													 })
+
+@login_required()
+def reporte_politicas_view(request):
+	title = "Reporte por políticas"
+	if request.user.groups.filter(name='Administrador').count() == 1:
+		politicas = PoliticaPublica.objects.all()
+		indicadores = Indicador.objects.all().order_by('id').select_related('politica_publica')
+	else:
+		return redirect('cuenta:home')
+
+	vigencia = Vigencia.objects.filter(activo=True).select_related('formulario').first()
+	preguntas = Pregunta.objects.filter(formulario=vigencia.formulario).order_by('id')
+	headers = []
+	for i in preguntas:
+		if i.id in [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,26, 27, 28, 29, 30, 31, 32, 33, 34, 35,36, 37, 38, 39]:
+			headers.append(i)
+	
+	respuestas = {}
+
+	for i in politicas:
+		formulario = FormularioRespuesta.objects.filter(vigencia=vigencia).filter(estado='aprobado').filter(politica_publica=i).prefetch_related('respuesta_set').first()
+		if formulario is not None:
+			with connection.cursor() as cursor:
+				cursor.execute("SELECT seguimiento_respuesta.pregunta_id, SUM(seguimiento_respuesta.valor::INTEGER) AS suma FROM seguimiento_formulariorespuesta INNER JOIN seguimiento_respuesta ON seguimiento_respuesta.formulario_respuesta_id = seguimiento_formulariorespuesta.id WHERE seguimiento_formulariorespuesta.id = %s AND seguimiento_respuesta.pregunta_id IN (9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,26, 27, 28, 29, 30, 31, 32, 33, 34, 35,36, 37, 38, 39) GROUP BY seguimiento_respuesta.pregunta_id order by seguimiento_respuesta.pregunta_id", [formulario.id])
+				rows = cursor.fetchall()
+				rows = dict(rows)
+				respuestas[i.id] = rows
+				
+	if request.user.groups.filter(name='Administrador').count() == 1:
+		extends = 'base/admin_nav.html'
+		
+	return render(request, "reportes/politicas.html", {"extends": extends,
+													 "title": title, 
+													 "politicas": politicas,
+													 "indicadores": indicadores,
+													 "headers": headers,
+													 "respuestas": respuestas
+													 })
+
+@login_required()
+def reporte_nivel1_view(request):
+	title = "Reporte general"
+	if request.user.groups.filter(name='Administrador').count() == 1:
+		politicas = PoliticaPublica.objects.all()
+		niveles1 = Nivel1.objects.all().order_by('id')
+	else:
+		return redirect('cuenta:home')
+
+	vigencia = Vigencia.objects.filter(activo=True).select_related('formulario').first()
+	preguntas = Pregunta.objects.filter(formulario=vigencia.formulario).order_by('id')
+	headers = []
+	for i in preguntas:
+		if i.id in [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,26, 27, 28, 29, 30, 31, 32, 33, 34, 35,36, 37, 38, 39]:
+			headers.append(i)
+	
+	respuestas = {}
+
+	for i in politicas:
+		formulario = FormularioRespuesta.objects.filter(vigencia=vigencia).filter(estado='aprobado').filter(politica_publica=i).prefetch_related('respuesta_set').first()
+		if formulario is not None:
+			tmp = formulario.respuesta_set.all()
+			prefetch_related_objects(tmp, 'indicador')
+			for item in tmp:
+				if item.indicador.nivel1 is not None:
+					with connection.cursor() as cursor:
+						cursor.execute("SELECT seguimiento_pregunta.id, SUM(seguimiento_respuesta.valor::INTEGER) AS sumatoria FROM seguimiento_formulariorespuesta INNER JOIN seguimiento_respuesta ON seguimiento_respuesta.formulario_respuesta_id = seguimiento_formulariorespuesta.id INNER JOIN seguimiento_pregunta ON seguimiento_respuesta.pregunta_id = seguimiento_pregunta.id INNER JOIN seguimiento_indicador ON seguimiento_respuesta.indicador_id = seguimiento_indicador.id INNER JOIN seguimiento_nivel1 ON seguimiento_indicador.nivel1_id = seguimiento_nivel1.id WHERE seguimiento_formulariorespuesta.id = %s AND seguimiento_nivel1.id = %s AND seguimiento_pregunta.id IN (9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,26, 27, 28, 29, 30, 31, 32, 33, 34, 35,36, 37, 38, 39) GROUP BY seguimiento_nivel1.id, seguimiento_pregunta.id order by seguimiento_pregunta.id", [formulario.id, item.indicador.nivel1.id])
+						rows = cursor.fetchall()
+						rows = dict(rows)
+						if rows is not None:
+							if formulario.politica_publica.id not in respuestas:
+								respuestas[formulario.politica_publica.id] = {}
+							respuestas[formulario.politica_publica.id][item.indicador.nivel1.id] = rows
+
+	if request.user.groups.filter(name='Administrador').count() == 1:
+		extends = 'base/admin_nav.html'
+		
+	return render(request, "reportes/nivel.html", {"extends": extends,
+													 "title": title, 
+													 "politicas": politicas,
+													 "niveles": niveles1,
+													 "headers": headers,
+													 "respuestas": respuestas
 													 })
